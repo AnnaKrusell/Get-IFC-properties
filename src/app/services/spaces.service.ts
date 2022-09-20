66bf29f9024c7b05e6618f6b54513a940ad05fc4
Anna Krusell (AKRU) <akru@niras.dk>
2022-09-20 11:25:15 +0200

    Added two queries for inserting uvalues

--- src/app/services/spaces.service.ts
+++ src/app/services/spaces.service.ts
@@ -5,22 +5,19 @@ import { Source, SourceType } from 'src/app/3rdparty/comunica/models';
 import { WKTObject, WKTObjectOptions } from 'ngx-ifc-viewer';
 import { lastValueFrom } from 'rxjs';
 import { Space } from '../models';
-
+import { AppComponent } from '../app.component';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-
 export class SpacesService {
   public queryResult?: any;
   public queryComplete: boolean = false;
 
-  constructor(
-    private _comunica: ComunicaService
-    ) { }
-    
-    async getSpaces(): Promise<Space[]>{
-      const query = `PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+  constructor(private _comunica: ComunicaService) {}
+
+  async getSpaces(): Promise<Space[]> {
+    const query = `PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
       PREFIX ifc: <http://ifcowl.openbimstandards.org/IFC2X3_Final#>
             
             SELECT DISTINCT ?type (GROUP_CONCAT(?wall) AS ?walls)
@@ -32,41 +29,83 @@ export class SpacesService {
             } GROUP BY ?type
       
        `;
-      const spaces = await lastValueFrom(this._comunica.selectQuery(query));
-      return spaces.map((item: any) => {
-        const URI = item.type.value;
-        const name = item.walls.value;
+    const spaces = await lastValueFrom(this._comunica.selectQuery(query));
+    return spaces.map((item: any) => {
+      const URI = item.type.value;
+      const name = item.walls.value;
 
-        return {URI, name};
-      })
-}
+      return { URI, name };
+    });
+  }
 
+  async insetUValues1(excelData: any) {
 
+    const query = `PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+    PREFIX ifc: <http://ifcowl.openbimstandards.org/IFC2X3_Final#>
+    PREFIX ex: <https://example.com/> 
+    
+    INSERT{ ?wall ex:uValue ?uValue } 
+    WHERE {
+    ?wall a ifc:IfcWallStandardCase ;
+            <https://web-bim/resources/referencePsetWallcommon> ?type .
+    
+    VALUES ( ?type ?uValue ) 
+    {("Basic Wall:Exterior - Brick on Block" 0.21 )
+     ("Basic Wall:Foundation - Concrete (417mm)"  0.18 ) }
+    } `;
 
-async insetUValues(){
+    this.queryResult = undefined;
+    this.queryComplete = false;
 
-  const query = `PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+    try {
+      this.queryResult = await this._comunica.updateQuery(query);
+      this.queryComplete = true;
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  async insetUValues(excelData: any) {
+
+    const query = `PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
   PREFIX ifc: <http://ifcowl.openbimstandards.org/IFC2X3_Final#>
   PREFIX ex: <https://example.com/>
   
-  INSERT DATA {<https://web-bim/resources/2O2Fr%24t4X7Zf8NOew3FNhv> ex:uValue 0.21 } 
+  INSERT DATA {<https://web-bim/resources/2O2Fr%24t4X7Zf8NOew3FNhv> ex:uValue ${excelData} } 
    `;
 
-  this.queryResult = undefined;
-  this.queryComplete = false;
+    this.queryResult = undefined;
+    this.queryComplete = false;
 
-  try{
+    try {
       this.queryResult = await this._comunica.updateQuery(query);
       this.queryComplete = true;
-  }catch(err){
+    } catch (err) {
       console.log(err);
+    }
   }
 
-}
+  async getUValues1(): Promise<Space[]> {
+    const query = `PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+    PREFIX ifc: <http://ifcowl.openbimstandards.org/IFC2X3_Final#>
+    PREFIX ex: <https://example.com/> 
+        
+    SELECT ?wall ?uValue ?type
+    WHERE{ ?wall ex:uValue ?uValue ; 
+                 <https://web-bim/resources/referencePsetWallcommon> ?type } 
+   `;
+    const values = await lastValueFrom(this._comunica.selectQuery(query));
+    return values.map((item: any) => {
+      const wall = item.wall.value;
+      const uValue = item.uValue.value;
+      const type = item.type.value;
 
+      return { wall, uValue, type };
+    });
+  }
 
-async getUValues(): Promise<Space[]>{
-  const query = `PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> 
+  async getUValues(): Promise<Space[]> {
+    const query = `PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> 
   PREFIX ifc: <http://ifcowl.openbimstandards.org/IFC2X3_Final#> 
   PREFIX kga: <https://w3id.org/kobl/geometry-analysis#> 
   PREFIX bot: <https://w3id.org/bot#>
@@ -84,16 +123,14 @@ async getUValues(): Promise<Space[]>{
    kga:area ?a .  
        } GROUP BY ?space }}
    `;
-  const values = await lastValueFrom(this._comunica.selectQuery(query));
+    const values = await lastValueFrom(this._comunica.selectQuery(query));
     return values.map((item: any) => {
       const space = item.space.value;
       const wallArea = item.wallArea.value;
       const uValue = item.uValue.value;
       const transmissionloss = item.transmissionloss.value;
 
-      return {space, transmissionloss, wallArea, uValue};
-  })
-}
-
-
+      return { space, transmissionloss, wallArea, uValue };
+    });
+  }
 }
