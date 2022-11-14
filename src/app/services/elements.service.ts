1bd5637f20490549e5e65aaeff401d6e44c802b6
akru <akru@niras.dk>
2022-11-14 11:16:18 +0100

    Changed instaces to pset

--- src/app/services/elements.service.ts
+++ src/app/services/elements.service.ts
@@ -17,25 +17,39 @@ export class ElementsService {
 
  
   async getProps(ifcTypeName: any): Promise<Properties[]> {
+    // const query = `PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+    // PREFIX ex: <https://example.com/>
+    // PREFIX ifc: <http://ifcowl.openbimstandards.org/IFC2X3_Final#>
+    // SELECT DISTINCT ?inst ?p ?o ?pSet  
+    // WHERE { 
+    //     ?inst a ifc:${ifcTypeName} .
+    //     ?inst ?p ?o .
+    //     ?p <https://example.com/belongsToPset> ?pSet .
+    // }`;
+
+
     const query = `PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-    PREFIX ex: <https://example.com/>
-    PREFIX ifc: <http://ifcowl.openbimstandards.org/IFC2X3_Final#>
-    SELECT DISTINCT ?inst ?p ?o
-    WHERE { 
-        ?inst a ifc:${ifcTypeName} .
-        ?inst ?p ?o .
-    }`;
+PREFIX ex: <https://example.com/>
+PREFIX ifc: <http://ifcowl.openbimstandards.org/IFC2X3_Final#>
+SELECT DISTINCT  ?pSet ?Property ?Value
+WHERE { 
+  ?inst a ifc:${ifcTypeName} .
+  ?inst ?Property ?Value .
+  ?Property <https://example.com/belongsToPset> ?pSet
+} ORDER BY ?pSet 
+    `;
 
     const spaces = await lastValueFrom(this._comunica.selectQuery(query));
     return spaces.map((item: any) => {
-      const instanceWithURI = item.inst.value;
-      const propertyWithURI = item.p.value;
-      const value = item.o.value;
+      // const instanceWithURI = item.inst.value;
+      const propertyWithURI = item.Property.value;
+      const value = item.Value.value;
+      const pSetWithURI = item.pSet.value;
 
-      var instance = instanceWithURI.split('/').pop();
+      var pSet = pSetWithURI.split('/').pop();
       var property = propertyWithURI.split('/').pop();
 
-      return { instance, property, value, ifcTypeName };
+      return { pSet, property, value, ifcTypeName };
     });
   }
 }
